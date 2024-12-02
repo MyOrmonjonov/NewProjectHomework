b0b52b516db9a04daee77cc26eb16297243c3b9f
Muhammadyusuf <muhammadyusufname@.com>
2024-12-03 03:12:13 +0500

    Birinchi page

--- src/main/java/org/example/demo6/ModulServelt.java
+++ src/main/java/org/example/demo6/ModulServelt.java
@@ -1,18 +1,31 @@
 package org.example.demo6;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/modul")
 public class ModulServelt extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String courseId = getInitParameter("courseId");
-        
+
+
+        try (EntityManager entityManager=MyListener.EMF.createEntityManager()){
+            Integer courseId = Integer.parseInt(getInitParameter("courseId"));
+            entityManager.getTransaction().begin();
+            List resultList = entityManager.createQuery("select m from Modul m where m.course=:courseId").getResultList();
+            entityManager.getTransaction().commit();
+
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }
